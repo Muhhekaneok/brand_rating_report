415eeb31a26d8859a276c9a7480287522b8824b1
darkwizard <wizardrad@gmail.com>
2025-10-19 19:13:53 +0700

    modified main.py

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-from data_processing import read_items_from_csv
-from reports import generate_report
+import argparse
 from tabulate import tabulate
 
-files = ["products1.csv", "products2.csv"]
+from data_processing import read_items_from_csv
+from reports import generate_report, REPORTS
 
-data_iter = read_items_from_csv(files)
-report_data, report_headers = generate_report("average-rating", data_iter)
-print(tabulate(report_data,
-               headers=report_headers,
-               showindex=range(1, len(report_data) + 1)))
 
-print("*" * 10)
-print(report_data, report_headers)
-print(report_data)
-print(report_headers)
+def main():
+    parser = argparse.ArgumentParser(
+        description="Generate product rating report from csv-files"
+    )
+
+    parser.add_argument(
+        "--files",
+        nargs="+",
+        required=True,
+        help="List of paths to csv-files to be processed"
+    )
+
+    parser.add_argument(
+        "--report",
+        choices=REPORTS.keys(),
+        required=True,
+        help="Report name to generate"
+    )
+
+    args = parser.parse_args()
+
+    data_iter = read_items_from_csv(args.files)
+
+    try:
+        report_data, report_header = generate_report(args.report, data_iter)
+
+        if report_data:
+            print(tabulate(
+                report_data,
+                headers=report_header,
+                showindex=range(1, len(report_data) + 1))
+            )
+
+    except ValueError as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
